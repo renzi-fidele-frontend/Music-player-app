5d07e03ca90d1260fb852931e929f91495a7128f
Renzi Fidele <renzifidele2001@gmail.com>
2023-10-21 23:30:16 +0300

    notificacao corrigida

--- src/components/Notificacao/Notificacao.jsx
+++ src/components/Notificacao/Notificacao.jsx
@@ -2,15 +2,20 @@ import React, { useContext, useEffect, useRef } from "react";
 import { musicContext } from "../../App";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { GoAlertFill } from "react-icons/go";
 
 const Notificacao = () => {
    const { estado, dispatch } = useContext(musicContext);
 
+   const erroId = "erro-id";
+   const sucessId = "sucesso-id";
+
    // Controlador da notificação
    useEffect(() => {
-      console.log(estado.musicaAtual[0]?.track?.preview_url , estado.musicaAtual.length > 0);
       if (estado.musicaAtual[0]?.track?.preview_url === null && estado.musicaAtual.length > 0) {
-         toast("A musica selecionada não possúi prévia");
+         toast("Prévia indisponível", { type: "warning", toastId: erroId });
+      } else if (estado.musicaAtual[0]?.track?.preview_url !== undefined) {
+         toast("Prévia disponível", { type: "success", toastId: sucessId });
       }
    }, [estado.musicaAtual]);
 
