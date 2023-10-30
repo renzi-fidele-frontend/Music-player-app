c58f41dff36ac1091a52f1375b556a3b61757abd
Renzi Fidele <renzifidele2001@gmail.com>
2023-10-30 03:11:40 +0200

    indo dormir

--- src/components/AlbumCard/AlbumCard.jsx
+++ src/components/AlbumCard/AlbumCard.jsx
@@ -3,6 +3,11 @@ import styles from "./AlbumCard.module.css";
 import { TbPlayerPlayFilled } from "react-icons/tb";
 
 const AlbumCard = ({ foto, nome, subtit, acao }) => {
+   //  Reduzindo o texto do conteúdo do post
+   function reduzir(str, maxlength) {
+      return str.length > maxlength ? str.slice(0, maxlength - 1) + "…" : str;
+   }
+
    return (
       <div
          className={styles.box}
@@ -16,7 +21,7 @@ const AlbumCard = ({ foto, nome, subtit, acao }) => {
          onMouse
       >
          <img src={foto} alt="Imagem do album" />
-         <h6>{nome}</h6>
+         <h6>{reduzir(nome, 17)}</h6>
          <p>{subtit}</p>
 
          <i>
