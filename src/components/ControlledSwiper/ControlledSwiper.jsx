ca939909cfaefb5895ebe327436e1194d281ac5c
Renzi Fidele <renzifidele2001@gmail.com>
2023-11-08 00:23:08 +0200

    Criando a estrutura do swiper

---
+++ src/components/ControlledSwiper/ControlledSwiper.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./ControlledSwiper.module.css";
+
+const ControlledSwiper = () => {
+   return <div id={styles.ct}>ControlledSwiper</div>;
+};
+
+export default ControlledSwiper;
