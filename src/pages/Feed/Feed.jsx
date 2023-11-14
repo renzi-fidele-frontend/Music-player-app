ce5f6c154ebfc85e3c193af860144ea08f24a075
Renzi Fidele <renzifidele2001@gmail.com>
2023-11-14 23:00:44 +0200

    Avancando com  a pagina de feed

--- src/pages/Feed/Feed.jsx
+++ src/pages/Feed/Feed.jsx
@@ -2,16 +2,33 @@ import React, { useContext, useEffect } from "react";
 import styles from "./Feed.module.css";
 import estiloBiblioteca from "../Biblioteca/Biblioteca.module.css";
 import { musicContext } from "../../App";
+import { FaSearch } from "react-icons/fa";
 
 const token = localStorage.getItem("token");
 
 const Feed = () => {
    const { estado, dispatch } = useContext(musicContext);
 
-
    return (
       <div id={styles.ct}>
-         <h2 className={estiloBiblioteca.tit1}>{`Artistas Top`}</h2>
+         <div id={styles.left}>
+            <div id={styles.search}>
+               <input type="text" name="pesquisa" id={styles.search} placeholder="Busque qualquer coisa" />
+               <button>
+                  <FaSearch />
+               </button>
+            </div>
+            <section>
+               <h2 className={estiloBiblioteca.tit1}>Feito para si</h2>
+            </section>
+            <section>
+               <div id={styles.left}>
+                  <h2 className={estiloBiblioteca.tit1}>Escute as melhores músicas de seus artistas favoritos</h2>
+               </div>
+               <div id={styles.right}></div>
+            </section>
+         </div>
+         <div id={styles.right}></div>
       </div>
    );
 };
---
+++ src/pages/Pesquisa/Pesquisa.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Pesquisa = () => {
+  return (
+    <div>Pesquisa</div>
+  )
+}
+
+export default Pesquisa
