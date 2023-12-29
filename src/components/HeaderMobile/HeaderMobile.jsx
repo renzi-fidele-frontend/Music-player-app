43aa5a10c81bb7fe264e0affd5e8498078e0b018
Renzi Fidele <renzifidele2001@gmail.com>
2023-12-29 17:47:47 +0200

    Avancando com o responsivo

--- src/components/HeaderMobile/HeaderMobile.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./HeaderMobile.module.css";
-
-const HeaderMobile = () => {
-   return <header id={styles.ct}>HeaderMobile</header>;
-};
-
-export default HeaderMobile;
