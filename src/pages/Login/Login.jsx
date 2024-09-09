a3bfb382acffec06552ea5624a0b6dc888b3d305
Renzi Fidele <renzifidele2001@gmail.com>
2024-09-10 00:21:55 +0200

    Adicionando o conteúdo PT da página de autenticação

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,22 +1,25 @@
 import styles from "./Login.module.css";
 import { RiSpotifyFill } from "react-icons/ri";
 import { loginEndPoint } from "../../../spotify";
+import { useTranslation } from "react-i18next";
 
 const Login = () => {
+   const { t } = useTranslation();
+
    return (
       <div id={styles.ct}>
          <i>
             <RiSpotifyFill />
          </i>
-         <a href={loginEndPoint}>Entrar</a>
+         <a href={loginEndPoint}>{t("pages.login.btn")}</a>
          <div id={styles.info}>
-            <h3>Memorize estes dados para fazer login</h3>
+            <h3>{t("pages.login.h3")}</h3>
             <div>
                <h6>Email:</h6>
                <p>testadorfy@outlook.com</p>
             </div>
             <div>
-               <h6>Senha:</h6>
+               <h6>{t("pages.login.h6")}</h6>
                <p>Spotify1234</p>
             </div>
          </div>
