c78d5a3db47f2999df8a9412c893ac6ae639951b
Renzi Fidele <renzifidele2001@gmail.com>
2024-09-10 00:12:38 +0200

    Adicionando a configuração inicial da internacionalização

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import i18n from "./i18n/i18n";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+   <React.StrictMode>
+      <Suspense fallback={<p>Loading...</p>}>
+         <App />
+      </Suspense>
+   </React.StrictMode>
+);
