c862ae91de2b67ce624d7e710f9a0c0e4c89d126
Renzi Fidele <renzifidele2001@gmail.com>
2024-10-31 23:55:08 +0200

    feat: Adicionando o tooltipo ao AlbumCard

--- src/components/AlbumCard/AlbumCard.jsx
+++ src/components/AlbumCard/AlbumCard.jsx
@@ -2,30 +2,32 @@ import styles from "./AlbumCard.module.css";
 import { TbPlayerPlayFilled } from "react-icons/tb";
 import Esqueleto from "../Skeletons/Esqueleto";
 import { reduzir } from "../../utils/reduzirTexto";
-
+import Tooltip from "../Tooltip/Tooltip";
 
 // varientes são: normal, swiper
 
 const AlbumCard = ({ foto, nome, subtit, acao, variante = "normal" }) => {
    return (
-      <div title={nome} className={`${styles.box} ${variante === "swiper" && styles.boxSwiper}`} onClick={acao}>
-         {foto && nome && subtit ? (
-            <>
-               <img loading="lazy" src={foto} alt="Imagem do album" />
-               <h6>{reduzir(nome, 17)}</h6>
-               <p>{subtit}</p>
-               <i>
-                  <TbPlayerPlayFilled />
-               </i>
-            </>
-         ) : (
-            <>
-               <Esqueleto tipo={"albumCard"} />
-               <Esqueleto tipo={"titulo"} />
-               <Esqueleto tipo={"texto"} />
-            </>
-         )}
-      </div>
+      <Tooltip conteudo={nome}>
+         <div className={`${styles.box} ${variante === "swiper" && styles.boxSwiper}`} onClick={acao}>
+            {foto && nome && subtit ? (
+               <>
+                  <img loading="lazy" src={foto} alt="Imagem do album" />
+                  <h6>{reduzir(nome, 17)}</h6>
+                  <p>{subtit}</p>
+                  <i>
+                     <TbPlayerPlayFilled />
+                  </i>
+               </>
+            ) : (
+               <>
+                  <Esqueleto tipo={"albumCard"} />
+                  <Esqueleto tipo={"titulo"} />
+                  <Esqueleto tipo={"texto"} />
+               </>
+            )}
+         </div>
+      </Tooltip>
    );
 };
 
